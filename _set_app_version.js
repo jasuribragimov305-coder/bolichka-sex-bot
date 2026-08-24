2ba8197625cdad4c8bd326805699e3c99befac4f
Jasur Ibragimov <jasuribragimov305@gmail.com>
2026-08-24 14:55:13 +0500

    Do'kon qo'shish oqimi bot orqali ham qo'shildi

---
+++ _set_app_version.js
@@ -0,0 +1,16 @@
+const admin = require("firebase-admin");
+const path = require("path");
+
+admin.initializeApp({
+  credential: admin.credential.cert(require(path.resolve(__dirname, "service-account.json"))),
+});
+
+async function main() {
+  await admin.firestore().collection("config").doc("appVersion").set({
+    latestBuild: 2,
+    downloadUrl: "https://raw.githubusercontent.com/jasuribragimov305-coder/bolichka-sex-bot/main/releases/bolichka-sex.apk",
+  });
+  console.log("Written.");
+}
+
+main().catch((e) => { console.error(e); process.exit(1); });
